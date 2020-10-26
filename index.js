29b67392e1beece6391db213f40074b4f7f2c6fc
Waldson Vital <waldson.vital@gmail.com>
2020-10-26 15:59:34 -0300

    busca de cliente/index e AR(implementando)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 //class
 const User = require('./lib/class/User')
 const Print = require('./lib/class/PrintPDF')
+const buscaCliente = require('./lib/services/BuscaCliente')
+
 global.ENV_SIGEP = "production"
 
 module.exports = {
@@ -13,6 +15,7 @@ module.exports = {
      * @param {Object} data { usuario, senha, codAdministrativo, idContrato, idCartaoPostagem, identificador, cepOrigem }
      */
     init: User.init,
+    buscaCliente,
     pdfPLP: (idPLP, PLP, path, filename) => {
         return new Print(idPLP, PLP, path, filename)
     }
